1110c5f650eee6baeec45260a39ebbf59feba8a4
etranchi <etranchi@student.42.fr>
2019-08-10 14:35:01 +0200

    add comment and function colleen c

--- C/Colleen.c
+++ C/Colleen.c
@@ -8,6 +8,14 @@ char space=32;
 char*com1="\n/*\n";
 char*com2="\n*/\n";
 /*
-	commentaire
+	outmain
 */
-int main(){char*a="#include<stdio.h>%cchar%ctab=9;%cchar%cparent=34;%cchar%cret=10;%cchar%cslash=92;%cchar*not_jump=%c%c%s%c;%cchar%cspace=32;%cchar*com1=%c%s/*%s%c;%cchar*com2=%c%s*/%s%c;%s%ccommentaire%sint main(){char*a=%c%s%c;printf(a, ret, space, ret, space, ret, space ,ret, space, ret, parent, slash, not_jump, parent, ret, space, ret, parent, not_jump, not_jump, parent, ret, parent, not_jump, not_jump, parent, com1, tab, com2, parent, a, parent, ret);return(0);};%c";printf(a, ret, space, ret, space, ret, space ,ret, space, ret, parent, slash, not_jump, parent, ret, space, ret, parent, not_jump, not_jump, parent, ret, parent, not_jump, not_jump, parent, com1, tab, com2, parent, a, parent, ret);return(0);};
+void put(char *str){printf(str, ret, space, ret, space, ret, space ,ret, space, ret, parent, slash, not_jump, parent, ret, space, ret, parent, not_jump, not_jump, parent, ret, parent, not_jump, not_jump, parent, com1, tab, com2, ret, com1, tab, com2, parent, str, parent, ret, ret, ret, ret);};
+int main(){
+/*
+	inmain
+*/
+char*str="#include<stdio.h>%cchar%ctab=9;%cchar%cparent=34;%cchar%cret=10;%cchar%cslash=92;%cchar*not_jump=%c%c%s%c;%cchar%cspace=32;%cchar*com1=%c%s/*%s%c;%cchar*com2=%c%s*/%s%c;%s%coutmain%svoid put(char *str){printf(str, ret, space, ret, space, ret, space ,ret, space, ret, parent, slash, not_jump, parent, ret, space, ret, parent, not_jump, not_jump, parent, ret, parent, not_jump, not_jump, parent, com1, tab, com2, ret, com1, tab, com2, parent, str, parent, ret, ret, ret, ret);};%cint main(){%s%cinmain%schar*str=%c%s%c;%cput(str);%creturn(0);%c};%c";
+put(str);
+return(0);
+};

adc79a59182320188f2616c942d9d419728aa56a
etranchi <etranchi@student.42.fr>
2019-10-15 16:15:50 +0200

    ca commence

--- ASM/Colleen/Colleen.s
+++ ASM/Colleen/Colleen.s
@@ -1,17 +1,25 @@
 ;Com
 section .data
-	str: db ";Com", 0x0A, "section .data", 0x0A, 0x09, "str: db", 0x22, "%s", 0x22, 0x0A, 0x09, "len: equ$-str",0x0A, "section .text", 0x0A, 0x09, "global _main", 0x0A, 0x09, "extern _printf", 0x0A, "_main:", 0x0A, 0
+	str: db ";Com %2$ssection .data%2$s%3$sstr: db=%s2$s%3$slen: equ$-str%2$ssection .text%2$s%3$sglobal _main%2$s%3$sextern _printf%2$s_main:", 0
 		.len: equ $ - str
-	fmt:    db "%s", 10, 0          ; The printf format, "\n",'0'
+	fmt:    db "%s", 0          ; The printf format, "\n",'0'
 
 section .text           ; Code section.
 	global _main		; the standard gcc entry point
 	extern _printf
 _main:				; the program label for the entry point
-	push    rbp		; set up stack frame, must be alligned
-	mov	rdi,str
-	mov	rsi,str
-	mov	rax,0		; or can be  xor  rax,rax
+	;push    rbp		; set up stack frame, must be alligned
+	;mov	rdi,fmt 	;first arg
+	;mov	rsi,str 	; second arg
+	;mov rdx,0x0A 	; third
+	;mov rcx,0x22	; fourth
+	;mov	rax,0		; or can be  xor  rax,rax
+	push rbp
+    mov rbp, rsp
+	lea rdi, [rel str]
+	mov rsi, 10 ; line feed
+	mov rdx, 9 	; tab
+	xor rax, rax
 	call   _printf		; Call C function
 	pop	rbp		; restore stack
 	mov	rax,0		; normal, no error, return value

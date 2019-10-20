0fe5c2fd05b27cdb582765dd924860f3c8276aaf
etranchi <etranchi@student.42.fr>
2019-10-20 20:54:17 +0200

    grace py ok

--- PY/Grace/Grace.py
+++ PY/Grace/Grace.py
@@ -1,10 +1,8 @@
-#!/Users/etranchi/.brew/Cellar/python/3.7.3/bin/python3
+#!/usr/local/bin/python3
 COUCOU = "COUCOU"
 AUREVOIR = "AUREVOIR"
 ADIEU = "ADIEU"
 def makeGrace():
-	print("Grace")
-
-
-
-makeGrace()
+	return '#!/Users/etranchi/.brew/Cellar/python/3.7.3/bin/python3\nCOUCOU = "COUCOU"\nAUREVOIR = "AUREVOIR"\nADIEU = "ADIEU"\ndef makeGrace():\n\treturn %r\nfp = open("Grace_kid.py","w+")\nfp.write(makeGrace()%%makeGrace())'
+fp = open("Grace_kid.py","w+")
+fp.write(makeGrace()%makeGrace())
